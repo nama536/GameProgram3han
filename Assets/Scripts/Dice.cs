73d60c291bd278fe56022125f8980ff9a54d91b6
yozi30 <kmhsnd25@gmail.com>
2026-01-15 12:26:49 +0900

    Dice
    サイコロの出目に応じてスプライトを変えてる途中です

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -10,11 +10,73 @@ public class Dice : MonoBehaviour
     private int dicevalue;
     private int sixdicevalue;
 
+    public Sprite newSprite; // インスペクターで設定するスプライト
+    private SpriteRenderer spriteRenderer;
+
+    //サイコロの出目画像
+    public Sprite one; // 1
+    public Sprite two; // 2
+    public Sprite three; // 3
+    public Sprite four; // 4
+    public Sprite five; // 5
+    public Sprite six; // 6
+
+    
+    public Sprite Riskone; // -1
+    public Sprite Risktwo; // -2
+    public Sprite Riskthree; // 3
+    public Sprite Riskfour; // -4
+    public Sprite Risksix; // 6
+    //-------------------------------------
+     void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>(); // SpriteRendererコンポーネントを取得
+        if (newSprite != null)
+        {
+            spriteRenderer.sprite = newSprite; // スプライトを割り当てて表示
+        }
+    }
+
+    void Update()
+    {
+        
+    }
     //普通のサイコロ
     private void OnDice(InputValue input)
     {
         //均等なダイス（1から最大値−１の間で生成）
         dicevalue = UnityEngine.Random.Range(1,7);
+
+        if(dicevalue == 1)
+        {
+            spriteRenderer.sprite == one;
+        }
+
+        else if(dicevalue == 2)
+        {
+            spriteRenderer.sprite == two;
+        }
+
+        else if(dicevalue == 3)
+        {
+            spriteRenderer.sprite == three;
+        }
+
+        else if(dicevalue == 4)
+        {
+            spriteRenderer.sprite == four;
+        }
+
+        else if(dicevalue == 5)
+        {
+            spriteRenderer.sprite == five;
+        }
+
+        else if(dicevalue == 6)
+        {
+            spriteRenderer.sprite == six;
+        }
+
         Debug.Log(dicevalue);
     }
 //-----------------------------------------------------------
@@ -22,7 +84,7 @@ public class Dice : MonoBehaviour
     //ハイリスクのサイコロ
     private void OnSixDice(InputValue input)
     {
-        sixdicevalue = UnityEngine.Random.Range(1,101);
+        sixdicevalue = UnityEngine.Random.Range(1,7);
         Select();
     }
 
@@ -30,27 +92,27 @@ public class Dice : MonoBehaviour
     void Select()
     {
         //1/100
-        if(sixdicevalue <= 17) //乱数で出てきた値が10以下なら 1 と判断
+        if(sixdicevalue <= 1) //乱数で出てきた値が10以下なら 1 と判断
         {
             View(-1);
         }
         //1/100
-        else if(sixdicevalue <= 34) //乱数で出てきた値が20以下 2 と判断
+        else if(sixdicevalue <= 2) //乱数で出てきた値が20以下 2 と判断
         {
             View(3);
         }
         //1/100
-        else if(sixdicevalue <= 51) //乱数で出てきた値が30以下なら 3 と判断
+        else if(sixdicevalue <= 3) //乱数で出てきた値が30以下なら 3 と判断
         {
             View(-2);
         }
         //1/100
-        else if(sixdicevalue <= 68) //乱数で出てきた値が40以下なら 4 と判断
+        else if(sixdicevalue <= 4) //乱数で出てきた値が40以下なら 4 と判断
         {
             View(6);
         }
         //1/100
-        else if(sixdicevalue <= 85) //乱数で出てきた値が50以下なら 5 と判断
+        else if(sixdicevalue <= 5) //乱数で出てきた値が50以下なら 5 と判断
         {
             View(-4);
         }
